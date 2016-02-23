ba97de178b82e8d6e8c547fd87b04d9e2f29837d
Guilherme Baptista <guisgb13@gmail.com>
2016-02-23 14:28:33 -0300

    Reporting call invitation canceled from UAS back to application layer when session negotiation fails

--- src/android/gov/nist/gnjvx/sip/stack/TCPMessageChannel.java
+++ src/android/gov/nist/gnjvx/sip/stack/TCPMessageChannel.java
@@ -265,7 +265,7 @@ public class TCPMessageChannel extends MessageChannel implements SIPMessageListe
             } catch (IOException ex) {
             }
             mySock = sock;
-            mySock.setSoTimeout(30000);
+            //mySock.setSoTimeout(30000);
             this.myClientInputStream = mySock.getInputStream();
             this.myClientOutputStream = mySock.getOutputStream();
             Thread thread = new Thread(this);
@@ -355,7 +355,7 @@ public class TCPMessageChannel extends MessageChannel implements SIPMessageListe
             }
 
             mySock = sock;
-            mySock.setSoTimeout(30000);
+            //mySock.setSoTimeout(30000);
             this.myClientInputStream = mySock.getInputStream();
             this.myClientOutputStream = mySock.getOutputStream();
             // start a new reader on this end of the pipe.
