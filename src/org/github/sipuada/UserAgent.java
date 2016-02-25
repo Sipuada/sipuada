375ecaa3e1424155c101fd14f5cbe3bbe777110e
Guilherme Baptista <guisgb13@gmail.com>
2016-02-25 11:02:50 -0300

    Playing safe, no longer ignoring some unexpected Sipuada-level exceptions and catching any exceptions that may happen when delegating execution to the registered Sipuada plug-ins
    Conflicts:
    	src/org/github/sipuada/Sipuada.java

--- src/org/github/sipuada/UserAgent.java
+++ src/org/github/sipuada/UserAgent.java
@@ -96,8 +96,6 @@ public class UserAgent implements SipListener {
 	private UserAgentServer uas;
 
 	private final Map<RequestMethod, SipuadaPlugin> registeredPlugins;
-	private final Map<String, SipuadaPlugin> activePlugins = new HashMap<>();
-
 	private final String rawAddress;
 
 	public UserAgent(SipProvider sipProvider, SipuadaListener sipuadaListener, Map<RequestMethod, SipuadaPlugin> plugins,
@@ -121,6 +119,10 @@ public class UserAgent implements SipListener {
 		registeredPlugins = plugins;
 		initSipuadaListener();
 		rawAddress = String.format("%s:%s/%s", localIp, localPort, transport);
+	}
+
+	protected SipProvider getProvider() {
+		return provider;
 	}
 
 	protected String getRawAddress() {
@@ -609,9 +611,14 @@ public class UserAgent implements SipListener {
 				if (event.getCallId().equals(callId)) {
 					wipeEstablishedCall(callId, eventBusSubscriberId);
 					eventBus.unregister(this);
-					activePlugins.remove(callId);
 					if (sessionPlugin != null) {
-						sessionPlugin.performSessionTermination(callId);
+						try {
+							sessionPlugin.performSessionTermination(callId);
+						} catch (Throwable unexpectedException) {
+							logger.error("Bad Sipuada plug-in crashed while trying " +
+									"to perform session termination in context of call {}.",
+									callId, unexpectedException);
+						}
 					}
 					listener.onCallFinished(callId);
 				}
@@ -620,8 +627,14 @@ public class UserAgent implements SipListener {
 		};
 		eventBus.register(eventBusSubscriber);
 		if (sessionPlugin != null) {
-			activePlugins.put(callId, sessionPlugin);
-			sessionPlugin.performSessionSetup(callId, this);
+			try {
+				sessionPlugin.performSessionSetup(callId, this);
+			} catch (Throwable unexpectedException) {
+				String error = "Bad Sipuada plug-in crashed while trying to perform" +
+						" session setup in context of call";
+				logger.error(String.format("%s {}.", error), callId, unexpectedException);
+				listener.onCallFailure(String.format("%s %s.", error, callId), callId);
+			}
 		}
 	}
 
